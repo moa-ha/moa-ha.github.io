1b4644f1c22be5eec288730a27a9bf256561b133
Moa Ha <moahaa03@gmail.com>
2024-05-01 22:09:28 +1200

    add home backgounr image

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,25 @@
 function Home() {
   return (
     <div className="container">
-      <div id="home">
-        <div className="home">
-          <p className="small-gap">Hello World! I'm </p>
-          <h1 className="small-gap">Moa Ha</h1>
-          <p className="small-gap">Full Stack Developer</p>
-          <a href="#about-me">
-            <button className="button">About Me</button>
-          </a>
+      <div className="home">
+        <p className="small-gap">Hello World! I'm </p>
+        <h1 className="small-gap">Moa Ha</h1>
+        <p className="small-gap">Full Stack Developer</p>
+        <a href="#about-me">
+          <button className="button">About Me</button>
+        </a>
 
-          <a href="#projects">
-            <button className="button" id="project">
-              Projects
-            </button>
-          </a>
-          <a
-            href="https://drive.google.com/file/d/1p27vWRIePgstw4dBp79gEKxeGFf_XJ4F/view?usp=sharing"
-            target="_blank"
-          >
-            <button className="button">Check My CV</button>
-          </a>
-        </div>
+        <a href="#projects">
+          <button className="button" id="project">
+            Projects
+          </button>
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1p27vWRIePgstw4dBp79gEKxeGFf_XJ4F/view?usp=sharing"
+          target="_blank"
+        >
+          <button className="button">Check My CV</button>
+        </a>
       </div>
     </div>
   )
